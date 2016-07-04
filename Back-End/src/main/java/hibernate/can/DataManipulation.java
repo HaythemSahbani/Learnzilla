0c544b1a6764a328de27a9448f53f23e92bd13ab
TwoBeer <kraus_000@Tobias>
2016-07-04 17:43:19 +0200

    jo

--- Back-End/src/main/java/hibernate/can/DataManipulation.java
+++ Back-End/src/main/java/hibernate/can/DataManipulation.java
@@ -1,5 +1,79 @@
 package hibernate.can;
 
-public class DataManipulation {
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.*;
+
+public class DataManipulation {
+	
+	public static void main(String[] args) {
+		
+		Benutzer b = new Benutzer();
+		b = login("horst","horst");
+	}
+	
+	//Hier wird die Connection geöffnet und eine Session zurückgegeben
+	public static Session getConnection()
+	{
+		Configuration cfg =new Configuration();
+		cfg.configure("hibernate.learnzilla.cfg.xml"); //populates the data of the configuration file
+		
+		//creating session factory object
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		//creating session object
+		Session session=factory.openSession();
+		
+		return session;		
+	}
+	
+	// Hier wird die Connection zur Datenbank geschlossen
+	public static void closeConnection(Session session)
+	{
+		session.close();
+	}
+	
+	public static Benutzer login(String benutzername, String passwort)
+	{
+		Session session;
+		session = getConnection();
+		
+		Transaction t= session.beginTransaction();
+		
+		Benutzer currentUser = new Benutzer();
+		
+		Query query = session.createQuery("FROM benutzer b WHERE b.benutzername =" + benutzername);
+		List results = query.list();
+		
+		System.out.println(results);
+		
+		return currentUser;
+	}
+	
+	
+	public int getFettigkeitsgrad()
+	{
+		Session session;
+
+		session = getConnection();
+		
+		Transaction t= session.beginTransaction();
+		
+		//Transaction macht iwas
+		//int f = benutzer.getFettigkeitsgrad();
+		//session.persist(benutzer);
+		
+		//Änderungen werden gespeichert
+		//t.commit();
+		
+		//Connection wird geschlossen und Session- Objekt übergeben
+		closeConnection(session);
+		
+		return 0;
+	}
+	
 }
---
+++ Back-End/src/main/java/hibernate/can/Test.java
@@ -0,0 +1,13 @@
+package hibernate.can;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//DataManipulation dmobject = new DataManipulation();
+		
+		//dmobject.login("horst","horst");
+	}
+
+}
